234bfc07b46c75d6afb600c32cb39b5409d8b2a8
Edis Leka <edisleka@yahoo.gr>
2025-01-08 16:41:08 +0200

    project startup directory sanity start

---
+++ app/(root)/page.tsx
@@ -0,0 +1,55 @@
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "John Doe" },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://images.unsplash.com/photo-1455849318743-b2233052fcff?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+      title: "We Robots",
+      category: "Robots",
+    },
+  ];
+
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Start Your Journey, <br /> Folk Them All
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia,
+          excepturi.
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+    </>
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https", // Match any HTTP/S protocol
+        hostname: "*", // Match all domains and subdomains
+      },
+    ],
+  },
 };
 
 export default nextConfig;
